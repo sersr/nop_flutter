528054a54cfb41b29988ac445e436d692b4f646c
sersr <1334349436@qq.com>
2022-06-19 15:55:13 +0800

    overlay 扩展

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,6 +1,7 @@
 export 'src/overlay/nav_overlay_getter.dart';
 export 'src/overlay/nav_overlay_mixin.dart';
 export 'src/overlay/overlay_event.dart';
+export 'src/overlay/overlay_ext.dart';
 export 'src/overlay/overlay_observer.dart';
 export 'src/overlay/overlay_pannel.dart';
 export 'src/overlay/overlay_side_pannel.dart';
